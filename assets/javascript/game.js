08fd6f49552116ebcc862e4e18c5d5368e629ef9
Alexis Frank <alexisfrank@alexisfrank-macbookpro2.roam.corp.google.com>
2017-05-11 07:29:19 +0200

    new version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,44 @@
 // Universal Variables
 
 var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var userInputArray = [''];
 var wins = 0;
 var loses = 0;
-console.log('This is the length of the letter array: ' + letters.length)
+var guessesLeft = 9;
 
 // Select random number with range length of array
 
 var randomNumber = function (range) {
 	return Math.floor(Math.random() * range.length);
-
 }
 
 // Computer's letter choice based selected index of selected number
 
-console.log('This is the value of randomNumber: ' + randomNumber(letters));
 var compChoice = letters[randomNumber(letters)];
 console.log('This is the value of compChoice: ' + compChoice);
 
-// User's letter choice
-var userInput = document.onkeyup = function(event){
-	console.log('This is the value of event.key: ' + event.key);
+// User's letter choice and array of user choices
+var userGuessArray = []
+var userGuess
+
+		document.onkeyup = function(event){
+        userGuess = event.key;
+        userGuessArray.push(userGuess);
+        console.log('This is the userGuess: ' + userGuess);
+        console.log('This is the userGuessArray: ' + userGuessArray)
+
+      }
+
+//Increment wins/loses, decrement guessesLeft
+
+	if (compChoice !== userGuess) {
+		loses++;
+		guessesLeft--;
 	}
-console.log('This is the value of userInput: ' + userInput);
 
-// Place user's letter choice in an array
-
-userInputArray = userInputArray.push(userInput);
-console.log('This is the value of userInputArray: ' + userInputArray);
-
-// Compaire computer's choice to user's choice
-
-if (compChoice != userInput){
-	loses = loses +1;
-} else {
-	wins = wins +1;
+	else {
+		wins++;
+		guessesLeft--;
+	}
 }
 
-console.log('This is the value of loses: ' + loses);
-console.log('This is the value of wins: ' + wins);
-
-
-// var guessedLetters = [''];
-// var userInput = document.onkeyup = function(event){
-// 	console.log(event.key);
-// 	}
-
-// 	guessedLetters.push(userInput);
-// 	console.log(guessedLetters);
+ 
